2e6fde4aed0f7317c80963abff9f0400fa848fc8
Akshay <akshay.maurya@nvest.in>
2024-11-28 13:24:57 +0530

    rizwan-latest

--- SBIG Simba/ViewController.swift
+++ SBIG Simba/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 import LocalAuthentication
-
+		
 class ViewController: UIViewController, WKNavigationDelegate ,WKScriptMessageHandler,
                         UNUserNotificationCenterDelegate
 {
